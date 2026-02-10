b77440f0c40deed7e2921f4288db6bb586ba46b5
joaojosers <joaojoserodeso@gmail.com>
2026-02-10 18:06:24 -0300

    fix(backend): módulo pacientes module e dependencias

--- backend/src/pacientes/pacientes.module.ts
+++ backend/src/pacientes/pacientes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PacientesController } from './pacientes.controller';
 import { PacientesService } from './pacientes.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PacientesController],
   providers: [PacientesService]
 })
